8321e2087b39d996c21948785c99826d988c8f59
DilushaC <dilushafernando97@gmail.com>
2021-10-05 09:33:20 +0530

    Position fixed

--- src/views/DqrCode.vue
+++ src/views/DqrCode.vue
@@ -1,66 +1,62 @@
 <template>
-<div>
-  <Header />
-  <div class="container">
+  <div>
+    <Header />
+    <div class="container">
+      <div class="row p-5">
+        <div class="col-6">
+          <h1>Good Morning Kaveen!</h1>
+        </div>
+      </div>
       <div class="row">
-      <div class="col-6">
-      <h1>Good Morning Kaveen!</h1>
+        <div class="col-11 rounded m-1  shadow-lg">
+          <div class="row">
+            <div class="col-5">
+              <div>
+                <img
+                  class="card-img-top"
+                  height="300px"
+                  src="https://wallpaperaccess.com/full/2213424.jpg"
+                  alt="Card image cap"
+                />
+                <div class="card-body">
+                  <h1>Kaveen Madushanka</h1>
+                </div>
+              </div>
+            </div>
+            <div class="col-6">
+              <h1>Scan Code Here</h1>
+              <h3>Or Scan the device</h3>
+              <br />
+              <hr />
+
+              <img
+                class="card-img-top"
+                height="300px"
+                width="100px"
+                src="https://blog.hubspot.com/hs-fs/hub/53/file-2457427390-jpg/00-Blog_Thinkstock_Images/qr-code.jpg?width=381&name=qr-code.jpg"
+                alt="Card image cap"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <br/>
-    <br/>
-    <div class="row">
-        <div class="col-11 rounded m-1  shadow-lg">
-        <div class="row">  
-        <div class="col-5">
-            <div>
-                <img class="card-img-top" height="300px" src="https://wallpaperaccess.com/full/2213424.jpg" alt="Card image cap">
-                <div class="card-body">
-                <h1>Kaveen Madushanka</h1>
-                </div>
-            </div>
-        </div>
-        <div class="col-6">  
-            <h1>Scan Code Here</h1>
-            <h3>Or Scan the device</h3>
-            <br/>
-            <hr/>
-
-            <img
-            class="card-img-top"
-            height="300px"
-            width="100px"
-            src="https://blog.hubspot.com/hs-fs/hub/53/file-2457427390-jpg/00-Blog_Thinkstock_Images/qr-code.jpg?width=381&name=qr-code.jpg"
-            alt="Card image cap"
-          />
-
-        
-    
-        </div>  
-        </div>      
-        </div>
-    </div>
-   </div>
-       <Footer />
+    <Footer />
   </div>
 </template>
 
 <script>
-
 // import QrcodeVue from 'qrcode.vue'
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 
 export default {
-    name: "DQRCode",
-components: {
+  name: "DQRCode",
+  components: {
     Header,
     Footer,
   },
-
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/EReqOT.vue
+++ src/views/EReqOT.vue
@@ -1,115 +1,111 @@
 <template>
-<div>
-  <Header />
-  <div class="container">
-    <div class="row">
-      <div class="col-6">
-      <h1>Good Morning Kaveen!</h1>
+  <div>
+    <Header />
+    <div class="container">
+      <div class="row p-5">
+        <div class="col-6">
+          <h1>Good Morning Kaveen!</h1>
+        </div>
       </div>
-    </div>
-
-    <div class="row">
-      <div class="col-11 rounded m-1  shadow-lg">
-        <div>
-          <div class="card-body">
+      <div class="row">
+        <div class="col-11 rounded m-1  shadow-lg">
+          <div>
+            <div class="card-body">
               <div class="row">
                 <div class="col-4">
-                    <h1>Add Credits</h1>
+                  <h1>Add Credits</h1>
                 </div>
-            </div>
-            <hr/>
-        <form>
-            <div class="form-group row">
-                <div class="col-md-4 p-2">
-                <label><b>Date :</b></label>
+              </div>
+              <hr />
+              <form>
+                <div class="form-group row">
+                  <div class="col-md-4 p-2">
+                    <label><b>Date :</b></label>
+                  </div>
+                  <div class="col-md-7 p-2">
+                    <input
+                      type="date"
+                      placeholder="Enter Date"
+                      class="form-control"
+                      v-model="c"
+                      required
+                    />
+                  </div>
                 </div>
-                <div class="col-md-7 p-2">
-                <input
-                    type="date"
-                    placeholder="Enter Date"
-                    class="form-control"
-                    v-model="c"
-                    required
-                />
-                </div>
-            </div>
 
-            <div class="form-group row">
-                <div class="col-md-4 p-2">
-                <label><b>Shift :</b></label>
+                <div class="form-group row">
+                  <div class="col-md-4 p-2">
+                    <label><b>Shift :</b></label>
+                  </div>
+                  <div class="col-md-7 p-2">
+                    <select class="form-select">
+                      <option value="" hidden>Select</option>
+                      <option value="day shift">day shift</option>
+                      <option value="night shift">night shift</option>
+                    </select>
+                  </div>
                 </div>
-            <div class="col-md-7 p-2">
-              <select class="form-select">
-                <option value="" hidden>Select</option>
-                <option value="day shift">day shift</option>
-                <option value="night shift">night shift</option>
-              </select>
-            </div>
-            </div>
 
-            <div class="form-group row">
-                <div class="col-md-4 p-2">
-                <label><b>Purpose :</b></label>
+                <div class="form-group row">
+                  <div class="col-md-4 p-2">
+                    <label><b>Purpose :</b></label>
+                  </div>
+                  <div class="col-md-7 p-2">
+                    <input
+                      type="text"
+                      placeholder="Enter Purpose"
+                      class="form-control"
+                      v-model="c"
+                      required
+                    />
+                  </div>
                 </div>
-                <div class="col-md-7 p-2">
-                <input
-                    type="text"
-                    placeholder="Enter Purpose"
-                    class="form-control"
-                    v-model="c"
-                    required
-                />
+
+                <div class="form-group row">
+                  <div class="col-md-4 p-2">
+                    <label><b>Route :</b></label>
+                  </div>
+                  <div class="col-md-7 p-2">
+                    <select class="form-select">
+                      <option value="" hidden>Select</option>
+                      <option value="Malabe_to_aturugiriya"
+                        >Malabe-aturugiriya</option
+                      >
+                      <option value="kandy_to_colombo">kandy-colombo</option>
+                      <option value="matara_to_galle">matara-galle</option>
+                    </select>
+                  </div>
                 </div>
-            </div>
-
-
-
-            <div class="form-group row">
-                <div class="col-md-4 p-2">
-                <label><b>Route :</b></label>
-                </div>
-            <div class="col-md-7 p-2">
-              <select class="form-select">
-                <option value="" hidden>Select</option>
-                <option value="Malabe_to_aturugiriya">Malabe-aturugiriya</option>
-                <option value="kandy_to_colombo">kandy-colombo</option>
-                <option value="matara_to_galle">matara-galle</option>
-
-              </select>
-            </div>
-            </div>
-          <div class="form-group">
-              <router-link :to="{ name: 'EAccount' }" class="btn btn-primary">
+                <div class="form-group">
+                  <router-link
+                    :to="{ name: 'EAccount' }"
+                    class="btn btn-primary"
+                  >
                     Request
-              </router-link>
-            <br/>   
-          </div>
-        </form>
+                  </router-link>
+                  <br />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-
-</div>
     <Footer />
   </div>
 </template>
 
 <script>
-
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 
 export default {
-    name: "EReqOt",
-    components: {
+  name: "EReqOt",
+  components: {
     Header,
     Footer,
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
